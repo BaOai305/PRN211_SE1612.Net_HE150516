b1c79a903d1a5bd74420e353650fcfa149bd376d
BaOai305 <orehx01@gmail.com>
2022-05-19 15:30:36 +0700

    create console project

---
+++ Inheritance_in_OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inheritance_in_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
