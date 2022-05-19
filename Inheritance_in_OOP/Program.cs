762ecd93e076e21da5f7f0f5f376be4788414722
BaOai305 <orehx01@gmail.com>
2022-05-19 15:50:31 +0700

    zx

--- Inheritance_in_OOP/Program.cs
+++ Inheritance_in_OOP/Program.cs
@@ -7,8 +7,12 @@ namespace Inheritance_in_OOP
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //print hello world
-            
+            // print 1 to 10
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
