6ed1cb00d0e647d82f8b003ae8a3af3313cd2108
BaOai305 <orehx01@gmail.com>
2022-06-23 17:16:01 +0700

    test name

--- Slot 18/Slot_18_19_20/ManageCategoriesApp/ManageCategories.cs
+++ Slot 18/Slot_18_19_20/ManageCategoriesApp/ManageCategories.cs
@@ -19,7 +19,7 @@ namespace ManageCategoriesApp
     {
         SqlConnection connection;
         SqlCommand command;
-        string ConnectionString = "Server=(local);uid=sa;pwd=123;database=MyStorem";
+        string ConnectionString = "Server=(local);uid=sa;pwd=123;database=MyStore";
         public List<Category> GetCategories()
         {
             List<Category> categories = new List<Category>();
