b6ecdf81827e6e332a4f93d4fb584e4f97b611e1
BaOai305 <orehx01@gmail.com>
2022-06-23 14:53:40 +0700

    all

---
+++ Assignment/Ass01Solution/BusinessObject/MemberObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BusinessObject
+{
+    public class MemberObject
+    {
+        public int MemberID { get; set; }
+        public string MemberName { get; set; }
+        //default: admin@fstore.com
+        public string Email { get; set; }
+        //default: admin@@
+        //Email, Password stored in the appsetting.json
+        public string Password { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
